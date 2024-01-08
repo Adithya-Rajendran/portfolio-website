1e24124c7329eed8718fcc43ccab4bb7aecacfec
Adithya Rajendran <adithyaraj@gmail.com>
2024-01-07 22:58:12 -0800

    added my image

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import heroImg from "@/public/Hero.jpg"
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -31,8 +32,8 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
+              src={heroImg}
+              alt="My portrait"
               width="192"
               height="192"
               quality="95"
