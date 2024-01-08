22f0344e4569cfd3b842e7572222385b30ea5f99
Adithya Rajendran <adithyaraj@gmail.com>
2024-01-08 00:07:17 -0800

    intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -63,11 +63,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">Hello, I'm Adithya Rajendran.</span> I'm a{" "}
+        <span className="font-bold">recent-graduate</span> with a{" "}
+        <span className="font-bold">Bachelors in Computer Science</span>. I enjoy
+        building and hacking <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">Python and Next.js</span>.
       </motion.h1>
 
       <motion.div
@@ -101,7 +101,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/adithya-rajendran/"
           target="_blank"
         >
           <BsLinkedin />
@@ -109,7 +109,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/Adithya-Rajendran"
           target="_blank"
         >
           <FaGithubSquare />
