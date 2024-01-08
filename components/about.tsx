ed60e610611b1d4db3702582cc8a3e0c1eb68501
Adithya Rajendran <adithyaraj@gmail.com>
2024-01-08 00:05:36 -0800

    about me

--- components/about.tsx
+++ components/about.tsx
@@ -20,29 +20,23 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
+        <span className="font-medium">Computer Science</span>, I continued to pursue my
+        passion for cybersecurity and programming. I completed industry-standard certifications namely,{" "}
+        <span className="font-medium">CompTIA Security+</span>.{" "} and {" "}
+        <span className="font-medium">AWS Solutions Architect</span>.{" "}
+        I am currenty pursuing the OffSec Certified Professional (OSCP).
+        <span className="italic">My favorite part of computing</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        feeling of figuring out a solution to a problem. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span>.
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        video games, watching movies, and tinkering in my home-lab. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="font-medium">photography</span>.
       </p>
     </motion.section>
   );
