1ee5ee17b84c50ad85dc2102a7c51cc8764076e5
Adithya Rajendran <adithyaraj@gmail.com>
2024-01-24 20:19:52 -0800

    imgur

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "blog.logrocket.com",
+                hostname: "i.imgur.com",
             },
         ],
     },
