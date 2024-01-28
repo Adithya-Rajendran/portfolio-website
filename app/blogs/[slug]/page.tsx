82fa85d9450529108a7cca32e22ea37c8c6f3f30
Adithya Rajendran <adithyaraj@gmail.com>
2024-01-28 12:43:42 -0800

    optimized the functions

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -4,21 +4,19 @@ import { getPostContent, getSlugs } from "@/context/markdown-posts";
 import ReactMarkdown from "react-markdown";
 import { components } from "@/components/blogs/md-components";
 import { Metadata } from "next";
-import { PostType } from "@/lib/types";
 import { notFound } from "next/navigation";
 import { Separator } from "@/components/ui/separator";
 
-async function validSlug(slug: string): Promise<boolean> {
-    const allSlugs = await getSlugs();
-    return allSlugs.includes(slug);
-}
-
-export default async function RemoteMdxPage({ params }: any) {
-    const valid = await validSlug(params.slug);
-    if (!valid) {
+export default async function RemoteMdxPage({
+    params,
+}: {
+    params: { slug: string };
+}) {
+    const { slug } = params;
+    const post = await getPostContent(slug);
+    if (!post) {
         return notFound();
     }
-    const { post } = await getPostData(params.slug);
     const { content } = post;
 
     return (
@@ -33,34 +31,28 @@ export default async function RemoteMdxPage({ params }: any) {
     );
 }
 
-export async function generateMetadata({ params }: any): Promise<Metadata> {
-    const valid = await validSlug(params.slug);
-    if (!valid) {
-        return {
-            title: "Not Found",
-        };
+export async function generateMetadata({
+    params,
+}: {
+    params: { slug: string };
+}): Promise<Metadata | undefined> {
+    const { slug } = params;
+    const post = await getPostContent(slug);
+    if (!post) {
+        return;
     }
-    const { metadata } = await getPostData(params.slug);
-    return metadata;
+    return {
+        title: post.title,
+        description: post.desc,
+    };
 }
 
 export const generateStaticParams = async () => {
     const slugs = await getSlugs();
+    if (!slugs) {
+        return;
+    }
     const paths = slugs.map((slug) => ({ slug: slug }));
 
     return paths;
 };
-
-async function getPostData(slug: string) {
-    const post: PostType = await getPostContent(slug);
-
-    const metadata: Metadata = {
-        title: post.title,
-        description: post.desc,
-    };
-
-    return {
-        post,
-        metadata,
-    };
-}
