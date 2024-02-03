2bedf7292adc4f1daff2a66e1a843db5aa044ef5
Adithya Rajendran <adithyaraj@gmail.com>
2024-02-03 12:50:48 -0800

    improved LCP

--- components/portfolio/intro.tsx
+++ components/portfolio/intro.tsx
@@ -1,90 +1,43 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
-import { motion } from "framer-motion";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
-import { useSectionInView } from "@/lib/hooks";
-import { useActiveSectionContext } from "@/context/active-section-context";
 import heroImg from "@/public/hero.webp";
 
 export default function Intro() {
-    const { ref } = useSectionInView("Home", 0.5);
-    const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
-
     return (
         <section
-            ref={ref}
             id="home"
             className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
         >
             <div className="flex items-center justify-center">
                 <div className="relative">
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{
-                            type: "tween",
-                            duration: 0.2,
-                        }}
-                    >
-                        <Image
-                            src={heroImg}
-                            alt="My portrait"
-                            width="192"
-                            height="192"
-                            quality={95}
-                            priority={true}
-                            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-                        />
-                    </motion.div>
-
-                    <motion.span
-                        className="absolute bottom-0 right-0 text-4xl"
-                        initial={{ opacity: 0, scale: 0 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{
-                            type: "spring",
-                            stiffness: 125,
-                            delay: 0.1,
-                            duration: 0.7,
-                        }}
-                    >
-                        👋
-                    </motion.span>
+                    <Image
+                        src={heroImg}
+                        alt="My portrait"
+                        width="192"
+                        height="192"
+                        quality={95}
+                        priority={true}
+                        className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+                    />
                 </div>
             </div>
 
-            <motion.h1
-                className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-            >
+            <h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
                 <span className="font-bold">Hello, I'm Adithya Rajendran.</span>{" "}
                 I'm a <span className="font-bold">recent-graduate</span> with a{" "}
                 <span className="font-bold">Bachelors in Computer Science</span>
                 . I enjoy building and securing{" "}
                 <span className="italic">sites & apps</span>. My focus is{" "}
                 <span className="underline">Python and Next.js</span>.
-            </motion.h1>
+            </h1>
 
-            <motion.div
-                className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                    delay: 0.1,
-                }}
-            >
+            <div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium">
                 <Link
-                    href="#contact"
+                    href="/#contact"
                     className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-                    onClick={() => {
-                        setActiveSection("Contact");
-                        setTimeOfLastClick(Date.now());
-                    }}
                 >
                     Contact me here{" "}
                     <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
@@ -116,7 +69,7 @@ export default function Intro() {
                 >
                     <FaGithubSquare />
                 </a>
-            </motion.div>
+            </div>
         </section>
     );
 }
