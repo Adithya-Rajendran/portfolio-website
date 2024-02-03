0e2ab8900a25af4bd261b42e6f09e5d585f7403a
Adithya Rajendran <adithyaraj@gmail.com>
2024-02-03 12:52:53 -0800

    removed auto hightlight

--- components/header.tsx
+++ components/header.tsx
@@ -30,7 +30,7 @@ export default function Header() {
                         >
                             <Link
                                 className={clsx(
-                                    "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                                    "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300 hover:bg-gray-100 hover:rounded-full dark:hover:bg-gray-800",
                                     {
                                         "text-gray-950 dark:text-gray-200":
                                             activeSection === link.name,
@@ -43,18 +43,6 @@ export default function Header() {
                                 }}
                             >
                                 {link.name}
-
-                                {link.name === activeSection && (
-                                    <motion.span
-                                        className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
-                                        layoutId="activeSection"
-                                        transition={{
-                                            type: "spring",
-                                            stiffness: 380,
-                                            damping: 30,
-                                        }}
-                                    ></motion.span>
-                                )}
                             </Link>
                         </motion.li>
                     ))}
