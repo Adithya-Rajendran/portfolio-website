cb83162b3c8bddfd01a95b8c822126ea6d446748
Adithya Rajendran <adithya@adithya-rajendran.com>
2026-03-03 06:56:51 +0000

    update resume links

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ export default async function Home() {
         <HomeClient
             skillCategories={skillCategories as any}
             certifications={certifications as any}
-            resumeUrl={intro?.resume?.asset?._ref || undefined}
             subtitle={intro?.subtitle}
             homeBio={intro?.homeBio as any}
         />
